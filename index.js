86bcb167f323af215886a04812fdd89b1415c87b
yardenSchwartz <yardensch95@gmail.com>
2021-07-27 13:00:01 +0300

    comment

--- index.js
+++ index.js
@@ -16,48 +16,6 @@ const sessions = require('./Routes/sessions');
 
 app.get('/keepalive', (req, res) => {
   res.send('The service up and ready for query');
-
-  // clicks_array = [];
-  // impressions_array = [];
-  // requests_array = [];
-
-  // let clicks_data = functions.extractData('./data/clicks.csv');
-  // clicks_data.forEach((row) => {
-  //   a = row.split(',');
-  //   let temp = {
-  //     TimeStamp: a[0],
-  //     SessionId: a[1],
-  //     Time: a[2],
-  //   };
-  //   clicks_array.push(temp);
-  //   // console.log(a);
-  // });
-
-  // let impressions_data = functions.extractData('./data/impressions.csv');
-  // impressions_data.forEach((row) => {
-  //   a = row.split(',');
-  //   let temp = {
-  //     TimeStamp: a[0],
-  //     SessionId: a[1],
-  //     Duration: a[2],
-  //   };
-  //   impressions_array.push(temp);
-  //   // console.log(a);
-  // });
-
-  // let requests_data = functions.extractData('./data/requests.csv');
-  // requests_data.forEach((row) => {
-  //   a = row.split(',');
-  //   let temp = {
-  //     TimeStamp: a[0],
-  //     SessionId: a[1],
-  //     Partner: a[2],
-  //     user_id: a[3],
-  //     bid: a[4],
-  //     win: a[5],
-  //   };
-  //   requests_array.push(temp);
-  // });
 });
 
 app.use('/userStats', users);
